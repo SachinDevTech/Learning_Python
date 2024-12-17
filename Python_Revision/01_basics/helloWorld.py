de365ec9d0e22b59c9e3ca5b0abc8240b93666eb
SachinDevTech <sachin.1107sk@gmail.com>
2024-12-17 22:10:17 +0530

    Add python working, pvm with example

---
+++ Python_Revision/01_basics/helloWorld.py
@@ -0,0 +1,9 @@
+print("Hellow World!")
+
+
+# function to sum two values
+def sum(a, b):
+    sum = a+b
+    return sum
+
+print(sum(12,12))
---
+++ Python_Revision/01_basics/usehelloWorld.py
@@ -0,0 +1,5 @@
+# importing the function "sum" from helloWorld
+from helloWorld import sum
+
+# printing the imported function
+print(sum(123,123))
