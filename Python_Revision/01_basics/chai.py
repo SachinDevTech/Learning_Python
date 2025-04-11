68f3432f6c789ce3589d7826c70129b18ac9b81f
SachinDevTech <sachin.1107sk@gmail.com>
2025-04-12 01:28:20 +0530

    new files added

---
+++ Python_Revision/01_basics/chai.py
@@ -0,0 +1,6 @@
+print("hi there")
+
+def printChai(n):
+    print("Your best tea is: " + n)
+    
+# printChai("Lemon Tea")
---
+++ Python_Revision/01_basics/useChai.py
@@ -0,0 +1,5 @@
+# importing module from another file
+from chai import printChai
+
+# Calling fxn printChai present in useChai.py file
+printChai("Ginger Tea!")
